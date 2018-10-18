438b1d9c679da6c0054ee2e163ed0c13bb683467
Maksim Likhanov <Maksim_Likhanov@epam.com>
2018-10-18 20:33:53 +0300

    added a new required field for builder

--- src/learning/patterns/creational/requied/fields/builder/App.java
+++ src/learning/patterns/creational/requied/fields/builder/App.java
@@ -6,6 +6,7 @@ package learning.patterns.creational.requied.fields.builder;
 public class App {
 
 	public static void main(String[] args) {
-		Address address = Address.builder().url("asd").port(90).build();
+		Address address = Address.builder().url("asd").name("asd").port(90).build();
+		Address address1 = Address.builder().url("asd").name("asd").port(90).description("lol").build();
 	}
 }
