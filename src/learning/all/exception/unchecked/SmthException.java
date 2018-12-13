e55ad58096276f2e9a58c5a43b478bdb159ea929
Maksim Likhanov <Maksim_Likhanov@epam.com>
2018-12-13 12:33:53 +0300

    smth

---
+++ src/learning/all/exception/unchecked/SmthException.java
@@ -0,0 +1,7 @@
+package learning.all.exception.unchecked;
+
+/**
+ * @author Maksim_Likhanov
+ */
+public class SmthException extends RuntimeException {
+}
---
+++ src/learning/all/strings/App.java
@@ -0,0 +1,11 @@
+package learning.all.strings;
+
+/**
+ * @author Maksim_Likhanov
+ */
+public class App {
+	public static void main(String[] args) {
+		String str = null;
+		System.out.println("String: " + str);
+	}
+}
